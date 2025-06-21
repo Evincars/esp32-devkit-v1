af74c3056902864697b2ef2973012bfab39a1125
lasakada <adam.lasak@tietoevry.com>
2025-06-22 01:47:43 +0200

    code refactor

--- src/index-html.cpp
+++ src/index-html.cpp
@@ -1,35 +1,41 @@
 #include "index-html.hpp"
 
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <title>Electronics Simplified</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,">
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 2.0rem;}
-    p {font-size: 3.0rem;}
-    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
-    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
-    .switch input {display: none}
-    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #2d7fc7; border-radius: 6px}
-    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
-    input:checked+.slider {background-color: #b30000}
-    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
-  </style>
-</head>
-<body>
-  <h2>Electronics Simplified</h2>
-  <h2>ESP Async Web Server</h2>
-  %BUTTONPLACEHOLDER%
-<script>function toggleCheckbox(element) {
-  var xhr = new XMLHttpRequest();
-  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
-  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
-  xhr.send();
-}
-</script>
-</body>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Evincars IoT</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:,">
+    <style>
+      html { font-family: Arial; display: inline-block; text-align: center; }
+      h2 { font-size: 2.0rem; }
+      p { font-size: 3.0rem; }
+      body { max-width: 600px; margin:0px auto; padding-bottom: 25px; background: #222; color: #eee; }
+      .switch { position: relative; display: inline-block; width: 120px; height: 68px } 
+      .switch input {display: none}
+      .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #2d7fc7; border-radius: 6px}
+      .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
+      input:checked+.slider {background-color: #b30000}
+      input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+    </style>
+  </head>
+  <body>
+    <h2>Evincars IoT</h2>
+    <h2>ESP32 Async Web Server</h2>
+    %BUTTONPLACEHOLDER%
+    <script>
+      function toggleCheckbox(element) {
+        var xhr = new XMLHttpRequest();
+        if(element.checked) { 
+          xhr.open("GET", "/update?output=" + element.id + "&state=1", true); 
+        }
+        else {
+          xhr.open("GET", "/update?output=" + element.id + "&state=0", true);
+        }
+        xhr.send();
+      }
+    </script>
+  </body>
 </html>
 )rawliteral";
--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,50 @@
-#include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <WiFi.h>
+
 #include "index-html.hpp"
 
-//Add your WiFi details
-const char* ssid = "Vysehrad";
-const char* password = "hesloheslo";
+// Add your WiFi details
+const char *wifiSSID = "Vysehrad";
+const char *wifiPassword = "hesloheslo";
 
-const char* PARAM_INPUT_1 = "output";
-const char* PARAM_INPUT_2 = "state";
+const char *PARAM_INPUT_1 = "output";
+const char *PARAM_INPUT_2 = "state";
 
 // Create an AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-String outputState(int output){
-  if(digitalRead(output)){
+String outputState(int output) {
+  if (digitalRead(output)) {
     return "checked";
-  }
-  else {
+  } else {
     return "";
   }
 }
 
 // Replaces placeholder with button section in your web page
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "BUTTONPLACEHOLDER"){
+String processor(const String &var) {
+  // Serial.println(var);
+  if (var == "BUTTONPLACEHOLDER") {
     String buttons = "";
-    buttons += "<h4>Output - GPIO 12</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"12\" " + outputState(12) + "><span class=\"slider\"></span></label>";
-    buttons += "<h4>Output - GPIO 13</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"13\" " + outputState(13) + "><span class=\"slider\"></span></label>";
-    buttons += "<h4>Output - GPIO 14</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"14\" " + outputState(14) + "><span class=\"slider\"></span></label>";
+    buttons +=
+        "<h4>Output - GPIO 12</h4><label class=\"switch\"><input "
+        "type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"12\" " +
+        outputState(12) + "><span class=\"slider\"></span></label>";
+    buttons +=
+        "<h4>Output - GPIO 13</h4><label class=\"switch\"><input "
+        "type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"13\" " +
+        outputState(13) + "><span class=\"slider\"></span></label>";
+    buttons +=
+        "<h4>Output - GPIO 14</h4><label class=\"switch\"><input "
+        "type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"14\" " +
+        outputState(14) + "><span class=\"slider\"></span></label>";
     return buttons;
   }
   return String();
 }
 
-void setup(){
+void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
 
@@ -45,9 +54,9 @@ void setup(){
   digitalWrite(13, LOW);
   pinMode(14, OUTPUT);
   digitalWrite(14, LOW);
-  
+
   // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
+  WiFi.begin(wifiSSID, wifiPassword);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
@@ -57,21 +66,22 @@ void setup(){
   Serial.println(WiFi.localIP());
 
   // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send_P(200, "text/html", index_html, processor);
   });
 
-  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+  // Send a GET request to
+  // <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request) {
     String inputMessage1;
     String inputMessage2;
-    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+    // GET input1 value on
+    // <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
     if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
       inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
       inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
       digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
-    }
-    else {
+    } else {
       inputMessage1 = "No message sent";
       inputMessage2 = "No message sent";
     }
@@ -86,6 +96,4 @@ void setup(){
   server.begin();
 }
 
-void loop() {
-
-}
+void loop() {}
